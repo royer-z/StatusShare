749730da64bc0363d65d8680cef7c74274e29318
royer-z <43219292+royer-z@users.noreply.github.com>
2019-02-23 19:03:07 -0500

    Task: assigned IDs to UI elements

--- app/src/main/java/com/example/statusshare/MainActivity.kt
+++ app/src/main/java/com/example/statusshare/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.statusshare
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mainEnterButton = findViewById(R.id.mainEnterButton) as Button
+
+        mainEnterButton.setOnClickListener {
+            setContentView(R.layout.activity_login)
+        }
     }
 }
