baf29c3cc7d9b8b50af1426addce359820a149e4
Erik <erik271f@gmail.com>
2019-03-15 12:33:23 -0400

    Task: update status

--- app/src/main/java/com/example/statusshare/EditProfileActivity.kt
+++ app/src/main/java/com/example/statusshare/EditProfileActivity.kt
@@ -1,12 +1,74 @@
 package com.example.statusshare
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_edit_profile.*
 
 class EditProfileActivity : AppCompatActivity() {
+
+    var mDatabase: DatabaseReference? = null
+    var mCurrentUser: FirebaseUser? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile)
+
+        if(intent.extras!=null){
+            var oldStatus = intent.extras.get("status")
+            var oldLocation = intent.extras.get("location")
+            var oldDestination = intent.extras.get("destination")
+
+            editProfileStatus.setText(oldStatus.toString())
+            editProfileLocation.setText(oldLocation.toString())
+            editProfileDestination.setText(oldDestination.toString())
+        }
+
+        editProfileSaveButton.setOnClickListener{
+            mCurrentUser = FirebaseAuth.getInstance().currentUser
+            var userId = mCurrentUser!!.uid
+
+            mDatabase = FirebaseDatabase.getInstance().reference.child("Registration q").child(userId)
+
+            var status = editProfileStatus.text.toString()
+            mDatabase!!.child("status").setValue(status).addOnCompleteListener{
+                task: Task<Void> ->
+                if (task.isSuccessful){
+                    Toast.makeText(this,"Status Updated Successfully!",Toast.LENGTH_LONG).show()
+                    startActivity(Intent(this,ProfileActivity::class.java))
+                } else {
+                    Toast.makeText(this,"Status Not Updated",Toast.LENGTH_LONG).show()
+                }
+            }
+
+            var location = editProfileLocation.text.toString()
+            mDatabase!!.child("location").setValue(location).addOnCompleteListener{
+                    task: Task<Void> ->
+                if (task.isSuccessful){
+                    Toast.makeText(this,"Location Updated Successfully!",Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(this,"Location Not Updated",Toast.LENGTH_LONG).show()
+                }
+            }
+
+            var destination = editProfileDestination.text.toString()
+            mDatabase!!.child("destination").setValue(destination).addOnCompleteListener{
+                    task: Task<Void> ->
+                if (task.isSuccessful){
+                    Toast.makeText(this,"Destination Updated Successfully!",Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(this,"Destination Not Updated",Toast.LENGTH_LONG).show()
+                }
+            }
+
+
+
+        }
     }
 }
--- app/src/main/java/com/example/statusshare/ProfileActivity.kt
+++ app/src/main/java/com/example/statusshare/ProfileActivity.kt
@@ -1,12 +1,59 @@
 package com.example.statusshare
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.*
+import com.google.firebase.storage.StorageReference
+import kotlinx.android.synthetic.main.activity_profile.*
 
+//import com.google.firebase.storage.StorageReference
 class ProfileActivity : AppCompatActivity() {
+
+    var mDatabase: DatabaseReference? = null
+    var mCurrentUser: FirebaseUser? = null
+    var mStorageRef:StorageReference?= null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
 
+
+        mCurrentUser = FirebaseAuth.getInstance().currentUser
+
+        var userID = mCurrentUser!!.uid
+
+        mDatabase = FirebaseDatabase.getInstance().reference
+            .child("Registration q")
+            .child(userID)
+
+        mDatabase!!.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                var user_status = dataSnapshot!!.child("status").value
+                var user_location = dataSnapshot!!.child("location").value
+                var user_destination = dataSnapshot!!.child("destination").value
+
+                profileStatus.text = user_status.toString()
+                profileLocationHeading.text = user_location.toString()
+                profileDestinationHeading.text = user_destination.toString()
+
+            }
+
+            override fun onCancelled(p0: DatabaseError) {
+
+            }
+        })
+
+
+        profileUpdateProfileButton.setOnClickListener{
+            var intent = Intent(this, EditProfileActivity::class.java)
+            intent.putExtra("status",profileStatus.text.toString())
+            intent.putExtra("location",profileLocationHeading.text.toString())
+            intent.putExtra("destination",profileDestinationHeading.text.toString())
+            startActivity(intent)
+        }
     }
 }
