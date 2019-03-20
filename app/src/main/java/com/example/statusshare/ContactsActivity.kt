a2d7dd0e224782b3d04808a79ef3418f0bf6a474
royer-z <43219292+royer-z@users.noreply.github.com>
2019-03-20 13:03:54 -0400

    Task: renamed Contacts activity files

--- app/src/main/java/com/example/statusshare/ContactsActivity.kt
+++ app/src/main/java/com/example/statusshare/ContactsActivity.kt
@@ -3,10 +3,10 @@ package com.example.statusshare
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
-class ContactActivity : AppCompatActivity() {
+class ContactsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.contact_activity)
+        setContentView(R.layout.activity_contacts)
 
     }
 
