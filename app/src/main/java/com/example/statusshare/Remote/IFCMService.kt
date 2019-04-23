240afaa83a71e100057250ca12a5d4994e06f45e
Anchundia11112 <Michaelanchundia6@gmail.com>
2019-04-23 19:39:31 -0400

    Mergering so that way I have newest version

--- app/src/main/java/com/example/statusshare/Remote/IFCMService.kt
+++ app/src/main/java/com/example/statusshare/Remote/IFCMService.kt
@@ -1,6 +1,7 @@
 package com.example.statusshare.Remote
 
 
+import com.example.statusshare.Model.MyResponse
 import com.example.statusshare.Model.Request
 import io.reactivex.Observable
 import retrofit2.http.Body
@@ -13,6 +14,6 @@ interface IFCMService {
             "Authorization:key=AAAA1gMhkSU:APA91bELNu-A-YZ0W-JTZhvpP98fOGHyDHZ8jB9pFwfLGj_kAtbpKKCl2oaCCo5br4tfNiom9ebr_8OudehoG0M2x2BgWL2Xd_-JFZ1KXgMKCaYKGIYuIY6-UGJXk4SPSwCZnyhzSfP4")
     @POST("fcm/send")
 
-    //fun sendFriendRequestToUser(@Body body: Request):Observable<MyResponse>
+    fun sendFriendRequestToUser(@Body body: Request):Observable<MyResponse>
 
 }
