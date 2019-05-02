4a5cdac29708df7bfed2d0827b9daf8a6f66cc4b
Erik <erik271f@gmail.com>
2019-05-02 11:01:35 -0400

    Current Status color shows up in user's edit profile page

--- app/src/main/java/com/example/statusshare/EditProfileActivity.kt
+++ app/src/main/java/com/example/statusshare/EditProfileActivity.kt
@@ -6,6 +6,7 @@ import android.app.AlertDialog
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.graphics.Color
 import android.location.Address
 import android.location.Geocoder
 import android.net.Uri
@@ -353,7 +354,7 @@ class EditProfileActivity : AppCompatActivity(), AdapterView.OnItemSelectedListe
         val pickerAdapter =
             CustomAdapter(this@EditProfileActivity, R.layout.status_availability_spinner, countryPickerData)
         statusSpinner.setAdapter(pickerAdapter)
-
+        val profileImage = findViewById<ImageView>(R.id.editProfileProfileImageButton)
         //statusSpinner.setSelection(1)
         mDatabase!!.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
@@ -362,20 +363,26 @@ class EditProfileActivity : AppCompatActivity(), AdapterView.OnItemSelectedListe
                 if (statusColorNum == "0") {
                     statusSpinner.setSelection(0)
                     status_name_code = countryPickerData.get(0).statusWord!!
+                    profileImage?.setBackgroundColor(Color.parseColor("#17A42F"))
                 }
                 if (statusColorNum == "1") {
                     statusSpinner.setSelection(1)
                     status_name_code = countryPickerData.get(1).statusWord!!
+                    profileImage?.setBackgroundColor(Color.YELLOW)
+
 
                 }
                 if (statusColorNum == "2") {
                     statusSpinner.setSelection(2)
                     status_name_code = countryPickerData.get(2).statusWord!!
+                    profileImage?.setBackgroundColor(Color.parseColor("#FFA500"))
+
                 }
 
                 if (statusColorNum == "3") {
                     statusSpinner.setSelection(3)
                     status_name_code = countryPickerData.get(3).statusWord!!
+                    profileImage?.setBackgroundColor(Color.RED)
                 }
             }
 
@@ -661,7 +668,8 @@ class EditProfileActivity : AppCompatActivity(), AdapterView.OnItemSelectedListe
                                 updateObj.put("image", downloadUriOrgiinal)
                                 updateObj.put("thumb_image", downloadUriThumb)
 
-
+                                var userId = mCurrentUser!!.uid
+                                mDatabase = FirebaseDatabase.getInstance().reference.child("Registration q").child(userId)
                                 mDatabase!!.updateChildren(updateObj)
                                     .addOnCompleteListener {
                                             task: Task<Void> ->
